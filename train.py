02aec9c5ca04f08b5c03056ba97ef03d876f6c5d
Gautham Venkataraman <gauthsvenkat99@gmail.com>
2022-05-14 14:50:55 +0200

    dataloader generates random data and training loop seems to work

---
+++ train.py
@@ -0,0 +1,34 @@
+from pytorch_metric_learning import losses
+from dataloader import WhaleDoDataset
+from models import WhaleDoModel
+from torch.utils.data import DataLoader
+import torch
+
+config = {
+    'csv_path': 'data/metadata.csv',
+    'backbone_model': 'resnet18',
+    'input_dim': (3, 443, 291),
+    'rep_dim': 512,
+    'pretrained': True,
+    'device': 'cpu',
+
+    'projector' : {
+        'hidden_dim': 1024,
+        'output_dim': 2
+    }
+}
+
+loss_func = losses.TripletMarginLoss()
+dataset = WhaleDoDataset(csv_path=config['csv_path'])
+train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+model = WhaleDoModel(config)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+for i, (x_batch, y_batch) in enumerate(train_loader):
+    optimizer.zero_grad()
+    embeddings = model(x_batch)
+    loss = loss_func(embeddings, y_batch)
+    loss.backward()
+    optimizer.step()
+
+    print(i)
