b37e7d44f7493ef3f14df4fecac8b136821d85a4
Gautham Venkataraman <gauthsvenkat99@gmail.com>
2022-05-14 14:50:55 +0200

    dataloader generates random data and training loop seems to work

--- dataloader.py
+++ dataloader.py
@@ -2,10 +2,16 @@ import torch
 from torchvision import transforms
 
 import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import LabelEncoder
 
 class WhaleDoDataset(torch.utils.data.Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
-        self.df = pd.read_csv(csv_file)
+    def __init__(self, csv_path, transform=None):
+        self.df = pd.read_csv(csv_path)
+        self.le = LabelEncoder()
+        self.labels = self.le.fit_transform(self.df['whale_id'])
+        self.transform = transform
         
         '''
         https://pytorch.org/tutorials/beginner/basics/data_tutorial.html#creating-a-custom-dataset-for-your-files
@@ -15,7 +21,7 @@ class WhaleDoDataset(torch.utils.data.Dataset):
 
         transformations is an optional set of augmentations that we can do
 
-        pick a decent height and width? (write a simple script to find the max width and height of the images)
+        pick a decent height and width? (write a simple script to find the max width and height of the images) 443, 291
 
         don't forget to rotate the left / right images!
 
@@ -23,3 +29,12 @@ class WhaleDoDataset(torch.utils.data.Dataset):
         '''
 
         pass
+
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx):
+        #just generate a random image and label for testing purposes
+        # do transformations here which is not implemented cause I'm just testing right now
+
+        return torch.rand((3,443,291)), self.labels[idx]
